6e8e0a3aa6ff6907a1d24721a984604375ce8f40
Vinicios Gomes <viniciosgomesviana@gmail.com>
2019-07-17 23:51:05 -0300

    Correcao de conexao com banco de dados

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -5,12 +5,20 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+  const { email } = req.body;
+
   try{
-    const user = User.create(req.body);
+
+    if(await User.findOne({ email }))
+      return res.status(400).send({ error: 'Email já registrado'});
+
+    const user = await User.create(req.body);
+
+    user.password = undefined;
 
     return res.send({ user });
   } catch (err) {
-    return res.status(400).send({ error: 'Registration failed'});
+    return res.status(400).send({ error: 'Falha de registro'});
   }
 });
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://sysadmin:sysadmin2019@ds363996.mlab.com:63996/heroku_890x66tk', { useMongoClient: true });
+const mongoURL = "mongodb://sysadmin:sysadmin2019@ds363996.mlab.com:63996/heroku_890x66tk";
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true, 
+  useCreateIndex: true
+});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
