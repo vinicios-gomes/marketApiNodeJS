ead7f79ec5c06dbbd246ea992482cc07e15933d1
Vinicios Gomes <viniciosgomesviana@gmail.com>
2019-07-18 00:49:50 -0300

    Adicinado rota base

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.get('/', (req, res) => {
+  res.send("Api rodando");
+});
 require('./app/controllers/authController')(app);
 require('./app/controllers/projectController')(app);
-
 
 app.listen(3333);
